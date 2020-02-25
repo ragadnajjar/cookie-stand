6abb2c9aac21ed7da09ae327ee47e4252a600515
ragadnajjar <ragadnajjar9@gmail.com>
2020-02-25 15:21:00 +0200

    new branch

--- js/app2.js
+++ js/app2.js
@@ -1,9 +1,5 @@
 
 'use strict';
-
-var allStores = document.getElementById('store');
-
-
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm'];
 
 var allStores = [];
@@ -15,9 +11,8 @@ function Store (location, minCust, maxCust, avgCookie) {
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgCookie = avgCookie;
-  this.hourSales = [];
+  this.resultArray = [];
   this.dailySales = 0;
-  allStores.push(this);
 }
 
 Store.prototype.renderRow = function(){
@@ -30,11 +25,11 @@ Store.prototype.renderRow = function(){
   trElement.appendChild(tdElement);
   for(var i = 0; i < hours.length; i++){
     tdElement= document.createElement('td');
-    tdElement.textContent = this.hourSales[i];
+    tdElement.textContent = this.resultArray[i];
     trElement.appendChild(tdElement);
   }
 
-
+  var allStores = document.getElementById('store');
   tdElement = document.createElement('td');
   tdElement.textContent = this.dailySales;
   trElement.appendChild(tdElement);
@@ -47,8 +42,9 @@ Store.prototype.randomNumber = function () {
 Store.prototype.populateHourlySales = function () {
   for (var i = 0; i < hours.length; i++) {
     var customersPerHour = Math.round(this.randomNumber() * this.avgCookie);
-    this.hourSales.push(customersPerHour);
-
+    this.resultArray.push(customersPerHour);
+function renderHours(){}
+var theadElement=document.getElementById ('store');
   }
 };
 
@@ -67,13 +63,13 @@ function renderHours() {
   thElement.textContent = 'Cookie Total';
   trElement.appendChild(thElement);
   theadElement.appendChild(trElement);
-
+}
 renderHours();
 
 Store.prototype.totalSold = function () {
   var cookiesSoldCalc = 0;
   for (var i = 0; i < hours.length; i++) {
-    cookiesSoldCalc += this.hourSales[i];
+    cookiesSoldCalc += this.resultArray[i];
   }
   this.dailySales = cookiesSoldCalc;
 };
@@ -85,22 +81,22 @@ var store1 = new Store('Seattle', 23, 65, 6.3);
 store1.randomNumber();
 store1.populateHourlySales();
 store1.totalSold();
-// store1.renderRow();
+store1.renderRow();
 var store2= new Store('Tokyo', 3, 24, 1.2);
 store2.randomNumber();
 store2.populateHourlySales();
 store2.totalSold();
-// store2.renderRow();
+store2.renderRow();
 var store3 = new Store('Dubai', 11, 38, 3.7);
 store3.randomNumber();
 store3.populateHourlySales();
 store3.totalSold();
-// store3.renderRow();
+store3.renderRow();
 var store4= new Store('Paris', 11, 38, 3.7);
 store4.randomNumber();
 store4.populateHourlySales();
 store4.totalSold();
-// store4.renderRow();
+store4.renderRow();
 var store5 = new Store('Lima', 20, 38, 2.3);
 store5.randomNumber();
 store5.populateHourlySales();
@@ -114,12 +110,11 @@ function addLocationSubmitted(event) {
     var allStores = new Store(formElement.storeName.value,
       Number(formElement.minimumCust.value),
       Number(formElement.maximumCust.value), Number(formElement.averageSales.value))
-  new.allStores.randomNumber();
-  new.allStores.populateHourlySales();
-  new.allStores.totalSold();
-  new.allStores.renderRow();
+  allStores.randomNumber();
+  allStores.populateHourlySales();
+  allStores.totalSold();
+  sallStores.renderRow();
 }
 
 var storeFormElement = document.getElementById('demo');
-storeFormElement.addEventListener('submit', addLocationSubmitted);
-}
+// done 
